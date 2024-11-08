c28590a97a6afd282b6f3a3b65b81d1f7645b181
Diego Roberto <diego-roberto@hotmail.com>
2024-11-08 17:08:47 -0300

    Commit inicial - quest start!

---
+++ desafio-ocorrencias/desafio-ocorrencias/src/main/java/com/bitbucket/diegoroberto/desafioocorrencias/DesafioOcorrenciasApplication.java
@@ -0,0 +1,13 @@
+package com.bitbucket.diegoroberto.desafioocorrencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioOcorrenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioOcorrenciasApplication.class, args);
+	}
+
+}
---
+++ desafio-ocorrencias/desafio-ocorrencias/src/test/java/com/bitbucket/diegoroberto/desafioocorrencias/DesafioOcorrenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitbucket.diegoroberto.desafioocorrencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioOcorrenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
