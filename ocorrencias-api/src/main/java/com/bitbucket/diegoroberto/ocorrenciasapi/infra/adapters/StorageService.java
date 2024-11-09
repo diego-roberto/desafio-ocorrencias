e1fe7348f49538b863750979e64934f3d29fedc7
Diego Roberto <diego-roberto@hotmail.com>
2024-11-09 00:24:51 -0300

    Configurações de Storage do MinIO

---
+++ ocorrencias-api/src/main/java/com/bitbucket/diegoroberto/ocorrenciasapi/infra/adapters/StorageService.java
@@ -0,0 +1,53 @@
+package com.bitbucket.diegoroberto.ocorrenciasapi.infra.adapters;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+
+@Service
+public class StorageService {
+
+    @Value("${minio.bucket.name}")
+    private String bucketName;
+
+    @PostConstruct
+    public void initializeBucket() {
+        try {
+            boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+            if (!found) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+                System.out.println("Bucket " + bucketName + " criado com sucesso.");
+            } else {
+                System.out.println("Bucket " + bucketName + " já existe.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao verificar ou criar o bucket: " + e.getMessage(), e);
+        }
+    }
+
+    @Autowired
+    private MinioClient minioClient;
+
+    public void uploadFile(String bucketName, String objectName, InputStream inputStream, String contentType) {
+        try {
+            boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+            if (!found) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            }
+            minioClient.putObject(
+                    PutObjectArgs.builder().bucket(bucketName).object(objectName).stream(
+                                    inputStream, inputStream.available(), -1)
+                            .contentType(contentType)
+                            .build());
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ocorreu ao realizar upload: " + e.getMessage());
+        }
+    }
+}
